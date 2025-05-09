59523ab380f8fc0037921a760c8ef4459f8efd46
Shubham <shubhambandkar08@gmail.com>
2025-05-09 17:30:10 +0530

    create new  collection

--- cypress/e2e/tests/CMSTest.cy.js
+++ cypress/e2e/tests/CMSTest.cy.js
@@ -1,9 +1,12 @@
 import { sessionPageObject } from "../../pages/sessionPage";
 import { createTag } from "../../pages/tag";
 import { createNewArticle } from "../../pages/article";
+import { createNCollection } from "../../pages/collection";
+
 const createTagObject = new createTag();
 const sessionPageObject1 = new sessionPageObject();
 const createNewArticle1 = new createNewArticle();
+const createCollection1 = new createNCollection();
 
 describe("Login Page", () => {
     beforeEach(() => {
@@ -11,29 +14,38 @@ describe("Login Page", () => {
         cy.viewport(1920, 1080);
         cy.session('loginSession', () => {
             sessionPageObject1.login();
-           
-        }); // <-- Properly close the cy.session() block
+        });
     });
 
     it("Verify Login", () => {
-        // Use the session to verify login
         sessionPageObject1.homeURL();
-        // cy.contains("Welcome").should("be.visible");
     });
 
     it("Verify create New tag", () => {
         sessionPageObject1.homeURL();
         cy.wait(2000);
-        createTagObject.createNewTag();   
+        createTagObject.createNewTag();
         cy.wait(2000);
-        // Add logic for verifying tags
     });
 
-    it.only("Verify create New Article",() => { 
+    it("Verify create New Article", () => {
         sessionPageObject1.homeURL();
         cy.wait(2000);
         createNewArticle1.createNewArticle();
-       
-
     })
-});
+
+    it('verify create New Collection', () => {
+        sessionPageObject1.homeURL();
+        cy.wait(2000);
+        createCollection1.createNewcollection();
+        cy.wait(2000);
+    })
+
+    it("Verify create New Collection", () => {
+        sessionPageObject1.homeURL();
+         createCollection1.ClickOncollectionMenuButton();
+        cy.wait(2000);  
+        createCollection1.enterCollectionName(createCollection1.name);
+       
+    })
+})
---
+++ cypress/pages/collection.js
@@ -0,0 +1,65 @@
+export class collectionPage {
+
+title = `Test Collection ${Math.floor(Math.random() * 900) + 100}`;
+name ='Videos'
+    collectionWebElemtents = {
+
+
+
+        clickOnCollMenu: "a[value='9'] span[class='ts-menu-label mui-1bw4j2m']",
+        clickOnAddNewCollButton: "[class='MuiButton-icon MuiButton-startIcon MuiButton-iconSizeMedium mui-ghkmmz']",
+        ETitle:'#\\:ri\\:',//cy.get()
+        ClickOnSubButton: "[type='submit']",
+        ClickAllTab:'#mui-p-99778-T-2',
+        searchBox:"[placeholder='Search collections...']",
+    }
+
+    ClickOncollectionMenuButton() {
+        cy.get(this.collectionWebElemtents.clickOnCollMenu).click({ force: true })
+    }
+    clickOnAddNewCollectionButton() {
+        cy.get(this.collectionWebElemtents.clickOnAddNewCollButton).click({ force: true })
+    }
+
+    enterTitle(title) {
+        cy.get(this.collectionWebElemtents.ETitle).type(title, { force: true })
+
+    }
+
+    clickOnSubmitButton() {
+        cy.get(this.collectionWebElemtents.ClickOnSubButton).click({ force: true })
+    }
+
+    ClickOnAllTab() {
+        cy.get(this.collectionWebElemtents.ClickAllTab).should('be.visible').click({ force: true })
+    }
+
+    enterCollectionName(name) {
+        cy.get(this.collectionWebElemtents.searchBox).type(name, { force: true })
+        .type('{enter}', { force: true })
+    }
+}
+
+export class createNCollection extends collectionPage {
+
+    createNewcollection() {
+        this.ClickOncollectionMenuButton();
+        this.clickOnAddNewCollectionButton();
+        cy.wait(2000);
+        this.enterTitle(this.title);
+        this.clickOnSubmitButton();
+        cy.wait(2000)
+        this.ClickOncollectionMenuButton();
+        cy.wait(3000)
+      //  this.ClickOnAllTab();
+      //  this.enterCollectionName(this.name)
+
+    }
+
+
+}
+
+
+
+
+
